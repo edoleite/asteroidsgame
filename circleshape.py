4fc097467f19985f80439896de5e7c2aa8dfcd3e
edoleite <edo_auhf@hotmail.com>
2025-05-14 20:36:59 +0300

    asteroids and collision logic added

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,7 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def is_collading(self, other_circleshape):
+        distance = pygame.math.Vector2.distance_to(self.position, other_circleshape.position)
+        return distance <= self.radius + other_circleshape.radius
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+import sys
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     print("Starting Asteroids!")
@@ -12,20 +15,56 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #Groups and containers
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatables, drawables)    
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = updatables
+
+    # initiating objects
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
+    
+    #starting game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0, 0, 0))
-        player.draw(screen)        
+        
+        for drawable in drawables:
+            drawable.draw(screen)        
+        
         pygame.display.flip()        
         dt = clock.tick(60) /1000
-        player.update(dt)
+        updatables.update(dt)
+
+        #checking for collisions
+        for asteroid in asteroids:
+            if asteroid.is_collading(player):
+                print("Game over!")
+                draw_game_over(screen)
+                pygame.display.flip()  # Update display
+                pygame.time.delay(3000)
+                pygame.quit()  # Ensures Pygame shuts down cleanly
+                sys.exit()
+
         
         
+def draw_game_over(screen):
+    pygame.font.init()
+    font = pygame.font.Font(None, 50)  # Use default font with size 50
+    text_surface = font.render("GAME OVER", True, (255, 0, 0))  # Red text
+    text_rect = text_surface.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2))
+    screen.blit(text_surface, text_rect)
+    pygame.display.flip()  # Update display
+
     
-# initiating a Player object
-player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
 
 
