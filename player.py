e4c1dddb1b2ce3b6f94885d1c589b15eeae7dc3d
edoleite <edo_auhf@hotmail.com>
2025-05-16 01:06:16 +0300

    shooting timer added.

--- player.py
+++ player.py
@@ -2,10 +2,13 @@ from circleshape import *
 from constants import *
 from shot import *
 
+
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
         
     
     # in the player class
@@ -25,6 +28,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -40,8 +44,10 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        shot = Shot(*self.position)
-        direction = pygame.Vector2(0, 1).rotate(self.rotation)
-        shot.velocity = direction * PLAYER_SHOOT_SPEED
+        if self.timer <= 0:
+            shot = Shot(*self.position)
+            direction = pygame.Vector2(0, 1).rotate(self.rotation)
+            shot.velocity = direction * PLAYER_SHOOT_SPEED
+            self.timer = PLAYER_SHOOT_COOLDOWN
 
     
